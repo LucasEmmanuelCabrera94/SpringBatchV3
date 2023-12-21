d517063d4f1ec00f2d990d0f0544a81f559c7e81
LucasEmmanuelCabrera94 <cabreraemmanuellucas@gmail.com>
2023-12-21 16:04:37 -0300

    cambio de nombre en script

--- src/main/resources/scriptcito.bat
+++ src/main/resources/scriptcito.bat
@@ -1,9 +1,9 @@
 @echo off
 
 set "JAR_PATH=C:\Users\lucas.cabrera\Documents\Backend\springbatch\out\artifacts\springbatch_jar\springbatch.jar"
-set "LOCATION_PATH=C:\Users\lucas.cabrera\Documents\Backend\springbatch\target\classes\application.properties"
+set "LOCATION_PATH=C:\Users\lucas.cabrera\Documents\Backend\springbatch\src\main\resources\conection.properties"
 
-java -jar "%JAR_PATH%" --spring.config.location=C:\Users\lucas.cabrera\Documents\Backend\springbatch\src\main\resources\prueba.properties
+java -jar "%JAR_PATH%" --spring.config.location="%LOCATION_PATH%"
 
 pause
 exit
